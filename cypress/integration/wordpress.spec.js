ca1a9f1f352ba0c6c0a8dc270f772e4b4f386f05
SHASHANKATOLE <shashankatole455@gmail.com>
2022-02-09 10:42:20 +0530

    update

--- cypress/integration/wordpress.spec.js
+++ cypress/integration/wordpress.spec.js
@@ -7,35 +7,24 @@ const word = new Wordpress()
 
 describe('wordpress validate functionality', () => {
 
-   
-
     beforeEach('Verify login functionality', () => {
-
         word.visit()
-
         word.login()
-
-        //check the backend was properly reached
-        // word.backend()
-
     })
 
-
-
-      
     it('Verify page navigating to the settings page', () => {
-         
-        word.backend()
+
+        word.backend()    //check the backend was properly reached
         word.settings_page()
     })
 
     it('Verify page navigating to the advanced ads page', () => {
-        
+
         word.advanced_ads_page()
     })
 
     it('Verify advanced ads list is empty', () => {
-        
+
         word.advanced_ads_list()
     })
 
